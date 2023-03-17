49ca9424651c6e5ecbbaca3ed4f87517e38509da
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-03-17 13:36:38 -0600

    enlazando

---
+++ src/lab/pkg8p2_lisadiazdelvalle_danielreyes/Lab8P2_LisaDiazdelValle_DanielReyes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg8p2_lisadiazdelvalle_danielreyes;
+
+/**
+ *
+ * @author HTS
+ */
+public class Lab8P2_LisaDiazdelValle_DanielReyes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
